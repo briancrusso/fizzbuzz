d908507c32c505cec7b3da85c2a835ee5a49025c
“briancrussogit config--global user.email russodraw@gmail.com <russodraw@gmail.com>
2015-05-13 13:56:38 -0400

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-for (var i = 1; i < 101; i++) {
+var userNum = prompt("Enter a number"); 
+
+for (var i = 0; i > userNum; i++) {
     if ((i % 3) === 0 && (i % 5) === 0) {
         console.log("FizzBuzz");
     } else if ((i % 5) === 0) {
         console.log("Buzz");
     } else if ((i % 3) === 0) {
-        console.log("Fizz");
+        console.log("Fizz"); 
     } else {
         console.log(i);
     }
 }
+
+
+
+	
